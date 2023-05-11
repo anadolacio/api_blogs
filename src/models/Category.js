c47f93cf6ba322a05ac4877a4f01a8d7a883521f
anadolacio <anadolacio@hotmail.com>
2023-05-11 17:23:47 -0300

    resolução requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+const CategorySchema = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: { type: DataTypes.STRING },
+        }, {
+        timestamps: false,
+        tableName: 'categories',
+        underscored : true,
+    })    
+    return Category;
+};
+
+module.exports = CategorySchema;
